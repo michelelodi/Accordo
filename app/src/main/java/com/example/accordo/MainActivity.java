e8d4c0f1e2644277601a36c2e77ed6a9a1a529e4
michi <michele.lodi@blu.it>
2021-06-05 18:47:03 +0200

    Imported Volley Library Registered new user on first run (on server)

--- app/src/main/java/com/example/accordo/MainActivity.java
+++ app/src/main/java/com/example/accordo/MainActivity.java
@@ -7,58 +7,82 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MainActivity extends AppCompatActivity {
 
-    SharedPreferences prefs;
-    SharedPreferences.Editor editor;
+    private SharedPreferences prefs;
+    private SharedPreferences.Editor editor;
+    private TextView t;
 
     private final String CURRENT_USER = "current_user";
-    private final String VERSION = "version_check";
-    private final String PREF_VERSION_CODE_KEY = "1.0";
+    private final String APP_PREFS = "accordo_prefs";
+    private final String PREF_VERSION_CODE_KEY = "version";
     private final int DOESNT_EXIST = -1;
-    private final String MYTAG = "MYTAG";
+    private final String TAG = "MYTAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        prefs = getSharedPreferences(VERSION,0);
+        prefs = getSharedPreferences(APP_PREFS,0);
         editor = prefs.edit();
+        t = findViewById(R.id.tt);
 
         checkFirstRun();
 
+        t.setText(prefs.getString(CURRENT_USER,"pippo"));
 
     }
 
     private void checkFirstRun() {
 
-
-
-        // Get current version code
         int currentVersionCode = BuildConfig.VERSION_CODE;
 
-        // Get saved version code
-        SharedPreferences prefs = getSharedPreferences(VERSION, MODE_PRIVATE);
+        SharedPreferences prefs = getSharedPreferences(APP_PREFS, MODE_PRIVATE);
         int savedVersionCode = prefs.getInt(PREF_VERSION_CODE_KEY, DOESNT_EXIST);
 
-        // Check for first run or upgrade
-        if (currentVersionCode == savedVersionCode) {
+        if (savedVersionCode == DOESNT_EXIST) {
 
-            //normal run
-            TextView t = findViewById(R.id.tt);
-            t.setText(prefs.getString(CURRENT_USER,"pippo"));
+            //first run
+            RequestQueue rq = Volley.newRequestQueue(getApplicationContext());
 
-        } else if (savedVersionCode == DOESNT_EXIST) {
-            // first run
-            editor.putString(CURRENT_USER, "michele");
+            final JSONObject jsonStudent = new JSONObject();
+            final String url = "https://ewserver.di.unimi.it/mobicomp/accordo/register.php";
+
+            JsonObjectRequest listRequest = new JsonObjectRequest(
+                    url, jsonStudent,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            try {
+                                editor.putString(CURRENT_USER, response.get("sid").toString()).apply();
+                                t.setText(response.get("sid").toString());
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+
+                public void onErrorResponse(VolleyError error) {
+                    Log.d(TAG, "REQUEST FAILED");
+                }
+            }
+            );
+            rq.add(listRequest);
 
         } else if (currentVersionCode > savedVersionCode) {
-            Log.d(MYTAG,"Upgraded run");
             // TODO This is an upgrade
         }
 
-        // Update the shared preferences with the current version code
         editor.putInt(PREF_VERSION_CODE_KEY, currentVersionCode).apply();
         return;
     }
